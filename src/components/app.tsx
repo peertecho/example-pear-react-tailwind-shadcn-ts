a960885d6b6196c8956d9ecc73e2bf6853448030
Marco <maidh91@gmail.com>
2025-03-04 10:43:31 +1100

    lint

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function App () {
   return (
-    <div className="p-[10px] bg-blue-700">
+    <div className='p-[10px] bg-blue-700'>
       <h1>MyApp</h1>
       <Button onClick={() => alert('hello')}>Add</Button>
     </div>
